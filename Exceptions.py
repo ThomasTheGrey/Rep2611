d50481763ac2624648e7c04ec8d5c97a0683f95d
Tom <herrschmidt@mai.de>
2024-11-26 11:16:31 +0100

    Teil 8 - fertig

--- Exceptions.py
+++ Exceptions.py
@@ -1,1 +1,25 @@
 #letzer Teil des Aufgabenzettels - Exceptions und Fehlerbehandlung
+
+
+
+try:
+    zahl = int(input("Bitte geben sie eine zahl ein:"))
+    teiler = int(input("Bitte gib einen Teiler ein:"))
+
+
+except:
+    print("Das ist keine Zahl!")
+    zahl = 1
+    teiler = 1
+
+try:
+    result = zahl/teiler
+    print (result)
+
+except:
+    print("Durch Null teilen darf man nicht!!")
+
+if (zahl < 0):
+    print("Achtung, die Zahl ist negativ!!")
+else:
+    print("die Zahl ist positiv, alles okay")
