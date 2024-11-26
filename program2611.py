e32ba7f5251b5cde3dd2e47144a34b97f3de4c21
Tom <herrschmidt@mai.de>
2024-11-26 09:11:07 +0100

    Leeres Programm + testkommentar

--- program2611.py
+++ program2611.py
@@ -1,3 +1,10 @@
 # Testcomment to check remote 2
+#Aufgabe 1
+startwert = 10
+zwischenwert = startwert + 5
+endwert = zwischenwert * 2
+print (F"{startwert},{zwischenwert},{endwert}")
+
+#Aufgabe 2
 
 
