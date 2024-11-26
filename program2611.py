fa3d5343f375fbed092fba32c68440b48a295ced
Tom <herrschmidt@mai.de>
2024-11-26 09:22:53 +0100

    Leeres Programm + testkommentar

--- program2611.py
+++ program2611.py
@@ -7,4 +7,25 @@ print (F"{startwert},{zwischenwert},{endwert}")
 
 #Aufgabe 2
 
+zahl = input("Bitte geben Sie eine Zahl ein:\n")
+
+try:
+    zahl = float(zahl)  # oder `int(eingabe)` für Ganzzahlen
+    print("Die Eingabe ist eine Zahl.")
+except ValueError:
+    print("Die Eingabe ist keine Zahl.")
+
+if (zahl % 2 == 0):
+    print("die Zahl ist gerade!")
+else: print("die Zahl ist ungerade")
+
+if (zahl > 100):
+    print("die Zahl ist größer als 100!")
+else:
+    if (zahl == 100):
+        print("die Zahl ist = 100!")
+    else:
+        print("die Zahl ist kleiner als 100!")
+
+
 
