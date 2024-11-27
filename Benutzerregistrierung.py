69f6161a413962c32ae8758ea6b701b45f4fe12e
Tom <herrschmidt@mai.de>
2024-11-27 13:38:01 +0100

    Aufgabenblatt 4 - Aufgabe 8 starting...

---
+++ Benutzerregistrierung.py
@@ -0,0 +1,7 @@
+""" 1.	Erstellen Sie ein Benutzerregistrierungssystem mit folgenden Funktionen:
+o	Benutzer registrieren (name, email, passwort in die Datenbank einfügen).
+o	Benutzer-Login:
+	Überprüfen Sie, ob email und passwort korrekt sind.
+o	Anzeigen aller registrierten Benutzer.
+"""
+
