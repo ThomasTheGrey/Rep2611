ca7aa49925ec05babe7dd236aab443f91c9b390a
Tom <herrschmidt@mai.de>
2024-11-27 09:24:32 +0100

    Aufgabenblatt 3 - Teil 2

---
+++ Schleifen02.py
@@ -0,0 +1,30 @@
+Zahlen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+# Aufgabe 4
+
+ZahlenA4 = Zahlen[0:10]
+
+for i in ZahlenA4:
+    print (i)
+
+# Aufgabe 5
+ZahlenA51 = Zahlen[1::2]
+
+for i in ZahlenA51:
+    print (i)
+
+ZahlenA52 = Zahlen[4::5]
+
+for i in ZahlenA52:
+    print (i)
+
+#Aufgabe 6
+
+for i in [1,2,3,4,5]:
+    print("")
+    for j in [1,2,3,4,5]:
+        print ("*", end="")
+
+
+# Aufgabe 7
+
