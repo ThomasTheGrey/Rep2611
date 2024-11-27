d6b760ce3b49a7431a0fbc1632827d4834e35021
Tom <herrschmidt@mai.de>
2024-11-27 11:17:02 +0100

    Aufgabenblatt 3 - Teil 4 komplett

--- For Schleifen Listen.py
+++ For Schleifen Listen.py
@@ -19,3 +19,23 @@ for i in wort:
     print (i)
 
 # Aufgabe 14
+
+ListeNeu = []
+for i in Liste:
+    if (i>10):
+        print (i)
+        ListeNeu.append(i)
+print (ListeNeu)
+
+#Aufgabe 15
+j = 0
+for i in ListeNeu:
+    print (f"Index ={j}", end="")
+    print (f"Wert ={i}")
+    j = j+1
+
+#Aufgabe 16
+
+for index, zahl in enumerate(ListeNeu):
+    print(f"Index: {index}, Wert: {zahl}")
+
