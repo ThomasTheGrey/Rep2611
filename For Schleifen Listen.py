da65c849a2883b426621addf993e8f4c987be8b7
Tom <herrschmidt@mai.de>
2024-11-27 09:51:36 +0100

    Aufgabenblatt 3 - Teil 4 start

---
+++ For Schleifen Listen.py
@@ -0,0 +1,21 @@
+# Aufgabe 11
+
+Liste = [10,20,30,40,50]
+
+for i in Liste:
+    print (i)
+
+# Aufgabe 12
+j = 0
+for i in Liste:
+    j = j+i
+print (j)
+
+# Aufgabe 13
+
+wort = input("Bitte geben sie ein Wort ein:\n")
+
+for i in wort:
+    print (i)
+
+# Aufgabe 14
