6df558428bacbba10ed23fc5efb6c6e15ff6954f
Tom <herrschmidt@mai.de>
2024-11-27 09:45:35 +0100

    Aufgabenblatt 3 - Teil 3 komplett

---
+++ Listen.py
@@ -0,0 +1,44 @@
+#Aufgabe 8
+
+Namensliste = ["Anna", "Max", "Tom","Lisa"]
+print (Namensliste)
+
+Namensliste = Namensliste + ["Marie"]
+print (Namensliste)
+
+Namensliste.remove("Tom")
+print (Namensliste)
+
+print (len (Namensliste))
+
+# Aufgabe 9
+
+Zahlen = [5,3,8,1,2]
+
+print (Zahlen)
+
+Zahlen.sort()
+
+print (Zahlen)
+
+Zahlen.reverse()
+
+print (Zahlen)
+
+
+# Aufgabe 10
+
+Zahlen02 = [1,2,3,4,5]
+
+print (Zahlen02)
+
+Zahlen02 = Zahlen02 + [6]
+
+print (Zahlen02)
+
+Zahlen02.insert(0,0)
+
+print (Zahlen02)
+
+print (Zahlen02.count(3))
+
