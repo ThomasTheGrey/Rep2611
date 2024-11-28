03ad28a6fe4bd0adc20c43b414060d6a101d179f
Tom <herrschmidt@mai.de>
2024-11-28 12:07:16 +0100

    Quizdatenbank - Frageauswertung

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,18 @@
+#Alte DB löschen
+import os
+import random
+
+# Name der Datenbankdatei
+datenbank_name = "questions.db"
+
+# Überprüfen, ob die Datei existiert
+if os.path.exists(datenbank_name):
+    os.remove(datenbank_name)  # Datei löschen
+    print(f"Datenbank '{datenbank_name}' wurde gelöscht.")
+else:
+    print(f"Datenbank '{datenbank_name}' existiert nicht.")
+
+
 # Erstellen einer Quizfragen Datenbank
 
 import sqlite3
@@ -14,10 +29,12 @@ CREATE TABLE IF NOT EXISTS questions (
     answer_wrong01 TEXT NOT NULL,
     answer_wrong02 TEXT NOT NULL,
     answer_wrong03 TEXT NOT NULL,
-    level INTEGER   
+    level TEXT NOT NULL   
 )
 """)
 connection.commit()
+
+
 
 # Funktion zur Frageerstellung, Eingabe durch den Nutzer
 
@@ -27,10 +44,10 @@ def frageerstellung():
     AntwortF1 = input("Bitte eine falsche Antwort (1) eigeben:\n")
     AntwortF2 = input("Bitte eine falsche Antwort (2) eigeben:\n")
     AntwortF3 = input("Bitte eine falsche Antwort (3) eigeben:\n")
-    Level = int(input("Wie schwierig zu beantworten (1-3) ist diese Frage?"))
+    Level = (input("Wie schwierig zu beantworten (A-C) ist diese Frage?"))
 
 
-    cursor.execute("INSERT INTO questions (question, answer_correct, answer_wrong01, answer_wrong02, answer_wrong03, level) VALUES (?, ?, ?, ?, ?)", (Frage, AntwortK, AntwortF1, AntwortF2, AntwortF3, Level))
+    cursor.execute("INSERT INTO questions (question, answer_correct, answer_wrong01, answer_wrong02, answer_wrong03, level) VALUES (?, ?, ?, ?, ?, ?)", (Frage, AntwortK, AntwortF1, AntwortF2, AntwortF3, Level))
 
 def fragen(n):
     cursor.execute("""
@@ -40,11 +57,25 @@ def fragen(n):
     """, (n,))
 
     result = cursor.fetchone()
+    Liste =[1,2,3,4]
+    random.shuffle(Liste)
+    print(f"Hier unsere erste Frage: {result[0]}")
+    antwort =input(f"Was ist die richtige Antwort?\n[a]{result[Liste[0]]}\n[b]{result[Liste[1]]}\n[c]{result[Liste[2]]}\n[d]{result[Liste[3]]}")
 
-    print(f"Hier unsere erste Frage: {result[1]}")
-    print(f"Was ist die richtige Antwort?\n[A]{result[2]}\n[B]{result[3]}\n[C]{result[4]}\n[D]{result[5]}")
+    if (antwort == "a" and Liste[0]==1):
+        print("Richtig!!)")
+    else:
+        if (antwort == "b" and Liste[1]==1):
+            print("Richtig!!)")
+        else:
+            if (antwort == "c" and Liste[2]==1):
+                print("Richtig!!)")
+            else:
+                if (antwort == "d" and Liste[3]==1):
+                    print("Richtig!!)")
+                else:
+                    print("Falsch!!")
 
-frageerstellung()
 frageerstellung()
 fragen(1)
 
